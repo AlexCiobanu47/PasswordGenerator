e0e550dc3fd5eeb0dfb6fa687011d58f8cd4b487
AlexCiobanu47 <alexciobanu72@yahoo.com>
2023-01-23 01:15:56 +0200

    Added symbols in password

--- src/Main.java
+++ src/Main.java
@@ -4,40 +4,45 @@ public class Main {
         //generate a random number that represents a char
         //26+26 lowercase and uppercase letters
         //10 digits
-        //15 symbols
-        //total of 77 characters(letters, digits and symbols)
-        int rand = (int) (Math.random()* 77);
+        //6 symbols
+        //total of 68 characters(letters, digits and symbols)
+        int rand = (int) (Math.random()* 68);
         //rand in [0-9] - digit -> ASCII [48-57]
         //rand in [10-35] - lowercase letter -> ASCII [97-122]
         //rand in [36- 61] - uppercase letter -> ASCII [65-90]
-        //rand in [62-76] - symbols -> ASCII [33-47]
+        //rand in [62-67] - symbols -> ASCII [33-38]
         if(rand <= 9){
             //digit
             rand += 48;
-            return (char)(rand); //return character for ASCII code rand
         }
         else if(rand <= 35){
             //uppercase
             rand +=55; //65-10
-            return (char)(rand);
         }
-        else{
+        else if(rand <= 61){
             //uppercase
             rand += 61; //97-36
-            return (char)(rand);
         }
         // TO DO: add symbols
-
+        else{
+            rand -= 29; // 62-33
+        }
+        return (char)(rand); //return character for ASCII code rand
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("What is the length of the password?");
+        System.out.println("How many passwords do you want to generate?");
+        int passwordsNumber = sc.nextInt();
+        System.out.println("What is the length of the passwords?");
         int passwordLength = sc.nextInt();
-        StringBuilder password = new StringBuilder();
-        for(int i = 0; i < passwordLength; i++){
-            password.append(generateCharacter());
+        for(int j = 0; j < passwordsNumber; j++){
+            StringBuilder password = new StringBuilder();
+            for(int i = 0; i < passwordLength; i++){
+                password.append(generateCharacter());
+            }
+            System.out.println(password);
         }
-        System.out.println(password);
     }
 
 }
